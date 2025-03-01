6087c59566671a91a14f765f9efb133542fe7435
aavvaavvaa <131255344+aavvaavvaa@users.noreply.github.com>
2025-03-01 22:53:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -12,5 +12,16 @@ function main() {
     document.querySelector(".gif_modal").addEventListener("click", function() {
         document.querySelector(".gif_modal").classList.remove("active");
     });
+    document.querySelectorAll(".tab-btn").forEach(button => {
+        button.addEventListener("click", function() {
+            // Убираем активный класс у всех кнопок и таблиц
+            document.querySelectorAll(".tab-btn").forEach(btn => btn.classList.remove("active"));
+            document.querySelectorAll(".table_container").forEach(table => table.classList.remove("active"));
+
+            // Делаем текущую кнопку и соответствующую таблицу активной
+            this.classList.add("active");
+            document.getElementById(this.dataset.target).classList.add("active");
+        });
+    });
 }
 main()
